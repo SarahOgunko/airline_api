f6a23722dd6fa544d21b035e16f1aa8d1c5c67b5
Sarah Ogunko <sarahogunko@hotmail.com>
2023-11-09 08:44:50 +0000

    corrected data loader

--- start_point/src/main/java/com/example/airline_api/components/DataLoader.java
+++ start_point/src/main/java/com/example/airline_api/components/DataLoader.java
@@ -6,10 +6,11 @@ import com.example.airline_api.repositories.FlightRepository;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataLoader {
+public class DataLoader  implements ApplicationRunner {
 
     @Autowired
     FlightRepository flightRepository;
@@ -17,7 +18,7 @@ public class DataLoader {
     @Autowired
     PassengerRepository passengerRepository;
 
-    public void run(ApplicationArguments args) throws Exception{
+    public void run(ApplicationArguments args) {
 
 //      Barcelona flight
 
